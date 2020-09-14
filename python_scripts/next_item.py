6acb44b65b13570ba9e78deeec8654ab69d079d2
Blogem <jochem@jochemvanleeuwen.nl>
2020-09-14 09:38:52 +0200

    added reverse and forward to the backend, added a script for dimming the lights, first (incomplete) draft of the rgb color wheel

--- python_scripts/next_item.py
+++ python_scripts/next_item.py
@@ -1,39 +1,99 @@
 # for a group of entities, return item current+1 or if not exists, item 0
 # do something with the entity, based on type
 
+def pulse(entity_id):
+  try:
+    # pulse: go to 100%, then 1% brightness, then back to original
+    state = hass.states.get(entity_id)
+    original_status = state.state
+
+    logger.info('Pulsing '+entity_id+'...')
+
+    if original_status == 'off':
+      data = { "entity_id": entity_id }
+      hass.services.call('light', 'turn_on', data)
+      time.sleep(1/10)
+
+    state = hass.states.get(entity_id)
+    original_brightness = state.attributes["brightness"]
+
+    logger.info('Original state: '+original_status+', original brightness: '+str(original_brightness)+')')
+      
+    # 100%
+    data = { "entity_id": entity_id, "brightness_pct": 100, "transition": 1}
+    hass.services.call('light', 'turn_on', data)
+    time.sleep(1)
+ 
+    # 1%
+    data = { "entity_id": entity_id, "brightness_pct": 1, "transition": 1}
+    hass.services.call('light', 'turn_on', data)
+    time.sleep(1)
+
+    # original reset
+    data = { "entity_id": entity_id, "brightness": original_brightness, "transition": 1}
+    hass.services.call('light', 'turn_on', data)
+    time.sleep(1)
+
+
+    if original_status == 'off':
+      data = { "entity_id": entity_id }
+      hass.services.call('light', 'turn_off', data)
+
+    return True
+
+  except Exception as e:
+    logger.error('Failed pulsing light!')
+    logger.info(e)
+    return False
+        
+
 current_tracker = 'input_text.current_active_light'
 groupname = data.get('groupname')
 counter = data.get('counter')
+direction = data.get('direction')
 
-
+# get next index in counter
 data = { "entity_id": counter }
-hass.services.call('input_number', 'increment', data)
-next = int(float(hass.states.get(counter).state))
 
-logger.info('Fetch next entity from group '+str(groupname)+' (next: '+str(next)+')')
+if direction == 'forward':
+  hass.services.call('input_number', 'increment', data)
+else: # reverse
+  hass.services.call('input_number', 'decrement', data)
 
+index = int(float(hass.states.get(counter).state))
+
+# see if we have to loop
 group = hass.states.get(groupname)
+num_entities = len(group.attributes["entity_id"])
 
-# loop around
-if next == len(group.attributes["entity_id"]):
-  next = 0
-  # reset input_number
+# loop forward
+if index == num_entities and direction == 'forward':
+  index = 0
   data = { "entity_id": counter, "value": 0}
   hass.services.call('input_number', 'set_value', data)
 
-entity_id = group.attributes["entity_id"][next]
+# loop backwards
+if index == 0 and direction == 'reverse':
+  data = { "entity_id": counter, "value": num_entities}
+  hass.services.call('input_number', 'set_value', data)
+
+logger.info('Fetch next entity from group '+str(groupname)+' (index: '+str(index)+')')
+
+entity_id = group.attributes["entity_id"][index]
 entity_type = entity_id.split('.')[0]
 entity_name = entity_id.split('.')[1]
+
 logger.info('Entity: '+str(entity_id)+', type:'+entity_type)
 
-# save the name of the entity so we can use it in hass later on
+# in case of switch or light, save the name of the entity so we can use it in hass later on
 if entity_type in ('switch', 'light'):
   data = { "entity_id": current_tracker, "value": entity_id}
   hass.services.call('input_text', 'set_value', data)
 
 # let the light announce itself
+status = hass.states.get(entity_id).state
 if entity_type == 'switch':
-  if hass.states.get(entity_id).state == 'on':
+  if status == 'on':
     # off-on
     data = { "entity_id": entity_id }
     hass.services.call('switch', 'turn_off', data)
@@ -49,17 +109,17 @@ if entity_type == 'switch':
     hass.services.call('switch', 'turn_off', data)
 
 elif entity_type == 'light':
-  if hass.states.get(entity_id).state == 'on':
-    # pulse
-    pass
-  else:
-    # on-pulse-off
-    data = { "entity_id": entity_id }
-    hass.services.call('light', 'turn_on', data)
-    # pulse
-    time.sleep(1)
-    data = { "entity_id": entity_id }
-    hass.services.call('light', 'turn_off', data)
+  # if status == 'on':
+  pulse(entity_id)
+
+  # else:
+  #   # on-pulse-off
+  #   # data = { "entity_id": entity_id }
+  #   # hass.services.call('light', 'turn_on', data)
+  #   pulse(entity_id)
+  #   # time.sleep(1)
+  #   # data = { "entity_id": entity_id }
+  #   # hass.services.call('light', 'turn_off', data)
 
 elif entity_type == 'script':
   # execute the script
